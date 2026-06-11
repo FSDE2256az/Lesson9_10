669dfd8fc68cad968485f2c1390719beda39ba75
Karimzada Kamran Natiq <karimzade_k@itstep.edu.az>
2026-06-11 21:49:09 +0400

    Task 1 completed

--- Lesson9_10/Program.cs
+++ Lesson9_10/Program.cs
@@ -76,10 +76,142 @@ internal class Program
         // Console.WriteLine(Operators.Nar);
         // Console.WriteLine(Operators.Garabagh);
 
-        int number = 0;
+        // int number = 0;
+        // 
+        // Console.WriteLine(number.IsEven());
+        // Console.WriteLine(number.ToBoolen());
 
-        Console.WriteLine(number.IsEven());
-        Console.WriteLine(number.ToBoolen());
+
+        // Task 1: 
+        // Shape => Area, ToString()
+
+        // Square    => P 
+        // Rectangle => P
+        // Circle
+
+
+        // ///////////////////////////////////////////////////////////
+
+        static void ChangeSize(Rectangle rectangle)
+        {
+            rectangle.Width = 50;
+        }
+
+        Shape[] shapes =
+        {
+            new Rectangle(5, 10),
+            new Square(5),
+            new Circle(5)
+        };
+
+        if (shapes[0] is Rectangle s)
+        {
+            ChangeSize(s);
+        }
+
+        foreach (var item in shapes)
+        {
+            //if (item is Rectangle st)
+            //    Console.WriteLine(st.Width);
+
+            Console.WriteLine(item);
+        }
 
     }
+}
+
+// ///////////////////////////////////////////////////////////
+
+public class Shape
+{
+    protected virtual double Area { get; set; }
+    public override string ToString() => $@"Area: {Area}";
+
+}
+
+public class Rectangle : Shape
+{
+    private double _width;
+    public virtual double Width
+    {
+        get { return _width; }
+        set
+        {
+            _width = value;
+            Area = _width * Height;
+        }
+    }
+
+
+    private double _height;
+    public virtual double Height
+    {
+        get { return _height; }
+        set
+        {
+            _height = value;
+            Area = _height * Width;
+        }
+    }
+
+
+    public Rectangle(double width, double height)
+    {
+        Width = width;
+        Height = height;
+
+        Area = Width * Height;
+    }
+
+    public override string ToString()
+    => $@"{base.ToString()}
+Heigth :{Height}
+Width: {Width}
+";
+
+}
+
+public class Square : Rectangle
+{
+    public Square(double side) : base(side, side)
+    {
+    }
+
+    public override double Height
+    {
+        get => base.Height;
+        set
+        {
+            base.Width = value;
+            base.Height = value;
+        }
+    }
+
+    public override double Width
+    {
+        get => base.Width;
+        set
+        {
+            base.Width = value;
+            base.Height = value;
+        }
+    }
+}
+
+
+public class Circle : Shape
+{
+    public double Radius { get; set; }
+
+    public Circle(double radius)
+    {
+        Radius = radius;
+        Area = radius * radius * 3.14;
+    }
+
+    public override string ToString()
+ => $@"{base.ToString()}
+Radius: {Radius}
+";
+
 }
---
+++ Lesson9_10/Task1/Task.cs
@@ -0,0 +1,96 @@
+﻿namespace Lesson9_10.Task1;
+
+
+public class Shape
+{
+    protected virtual double Area { get; set; }
+    public override string ToString() => $@"Area: {Area}";
+
+}
+
+public class Rectangle : Shape
+{
+    private double _width;
+    public virtual double Width
+    {
+        get { return _width; }
+        set
+        {
+            _width = value;
+            Area = _width * Height;
+        }
+    }
+
+
+    private double _height;
+    public virtual double Height
+    {
+        get { return _height; }
+        set
+        {
+            _height = value;
+            Area = _height * Width;
+        }
+    }
+
+
+    public Rectangle(double width, double height)
+    {
+        Width = width;
+        Height = height;
+
+        Area = Width * Height;
+    }
+
+    public override string ToString()
+    => $@"{base.ToString()}
+Heigth :{Height}
+Width: {Width}
+";
+
+}
+
+public class Square : Rectangle
+{
+    public Square(double side) : base(side, side)
+    {
+    }
+
+    public override double Height
+    {
+        get => base.Height;
+        set
+        {
+            base.Width = value;
+            base.Height = value;
+        }
+    }
+
+    public override double Width
+    {
+        get => base.Width;
+        set
+        {
+            base.Width = value;
+            base.Height = value;
+        }
+    }
+}
+
+
+public class Circle : Shape
+{
+    public double Radius { get; set; }
+
+    public Circle(double radius)
+    {
+        Radius = radius;
+        Area = radius * radius * 3.14;
+    }
+
+    public override string ToString()
+ => $@"{base.ToString()}
+Radius: {Radius}
+";
+
+}
